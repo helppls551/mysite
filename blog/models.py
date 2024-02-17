56539826b6e537714981db5647d10da7d9ac98ff
unknown <grinat234@gmail.com>
2024-02-17 11:05:56 +0300

    comm

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import  settings
+from django.utils import  timezone
+# Create your models here.
+
+
+class MyPublish(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    photo = models.ImageField(upload_to='img/%Y/%m/%d')
+    publish_date = models.DateTimeField(blank=True,null=True)
+    create_date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.publish_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
