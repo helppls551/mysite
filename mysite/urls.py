3fe0b45922b211ac4aaf3157dcc504e40cd3d350
mikail <gngitrg@mail.ru>
2024-03-02 11:18:00 +0300

    new

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', index),
+    path('', hi)
 ]
