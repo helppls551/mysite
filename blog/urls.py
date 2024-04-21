7824ec21385b2d8662b308acb2034a9966a3f295
rinat <amadishams@gmail.com>
2024-04-21 11:10:52 +0300

    test

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('hi/', views.hi, name = 'hi'),
+    path("categ/<int:catid>/",views.categ,name ='categ')
+]
---
+++ blog/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponseNotFound, HttpResponse, Http404
+from .models import MyPublish
+# Create your views here.
+
+
+def index(request):
+    posts = MyPublish.objects.all()
+    print(posts)
+    print('>>>')
+    return render(request, 'blog/index.html',{'posts': posts})
+
+
+def hi(request):
+    return render(request, 'blog/hi.html')
+
+
+def categ(request, catid):
+    if request.GET:
+        print('Имя:', request.GET['name'])
+        print('Возраст:', request.GET['age'])
+    # if request.GET:
+    #     if request.GET['password'] == '12345':
+    #         print('Доступ открыт')
+    #     else:
+    #         print('Иди нахуй сука')
+    #     print(request.GET)
+    # else:
+    #     print('Нет данных')
+    if int(catid) == 50:
+        raise Http404 # ручная генерация ошибки
+    if int(catid) == 1000:
+        return redirect('home')  # перенаправление
+    if int(catid) == 600:
+        return redirect('hi', permanent=True)
+    return HttpResponse(f"<h1>Page {catid}</h1>")
+
+
+def pageNotFound(request, exception):
+    print(exception, "***")
+    return HttpResponseNotFound('<h1>Страница не найдена:(</h1>')
